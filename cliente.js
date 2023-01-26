003f313d945fa99a2c37525316073b0f13d49b35
Braian Perea <braianperea@gmail.com>
2023-01-26 16:56:14 -0300

    primer commit

---
+++ cliente.js
@@ -0,0 +1,13 @@
+class Pedido{
+    constructor(nombre_tienda,cliente,direccion_facturacion,direccion_envio,datos_facturacion,detalle_pedido,cobranza,producto){
+        this.tienda = nombre_tienda
+        this.cliente = cliente,
+        this.direccion_facturacion = direccion_facturacion,
+        this.envio = direccion_envio,
+        this.facturacion = datos_facturacion,
+        this.detalle = detalle_pedido,
+        this.cobranza = cobranza,
+        this.producto = producto
+    }
+    
+}
